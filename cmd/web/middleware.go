6aa6433f52fc3fef83d4782e38c98b8f1b6dcb24
Rostislav Pylypiv <rostislav.pylypiv@gmail.com>
2022-10-03 16:58:24 +0300

    Added SessionLoad

--- cmd/web/middleware.go
+++ cmd/web/middleware.go
@@ -1,18 +1,19 @@
 package main
 
 import (
-	"fmt"
 	"github.com/justinas/nosurf"
 	"net/http"
 )
 
-func WriteToConsole(next http.Handler) http.Handler {
-	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		fmt.Println("Hit the page")
-		next.ServeHTTP(w, r)
-	})
-}
+// Middleware example
+//func WriteToConsole(next http.Handler) http.Handler {
+//	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+//		fmt.Println("Hit the page")
+//		next.ServeHTTP(w, r)
+//	})
+//}
 
+// NoSurf adds CSRF protection to all POST requests
 func NoSurf(next http.Handler) http.Handler {
 	csrfHandler := nosurf.New(next)
 
@@ -23,4 +24,9 @@ func NoSurf(next http.Handler) http.Handler {
 		SameSite: http.SameSiteLaxMode,
 	})
 	return csrfHandler
+}
+
+// SessionLoadloads and saves sessionon every request
+func SessionLoad(next http.Handler) http.Handler {
+	return session.LoadAndSave(next)
 }
