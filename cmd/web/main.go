97cfc015f9b2ca4c0063fbd1007bbd5bdeffe133
Rostislav Pylypiv <rostislav.pylypiv@gmail.com>
2022-10-03 16:53:38 +0300

    Added sessions

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -2,18 +2,32 @@ package main
 
 import (
 	"fmt"
+	"github.com/alexedwards/scs/v2"
 	"github.com/rostis232/awesomeProject/pkg/config"
 	"github.com/rostis232/awesomeProject/pkg/handlers"
 	"github.com/rostis232/awesomeProject/pkg/render"
 	"log"
 	"net/http"
+	"time"
 )
 
 const portNumber = ":8080"
 
+var app config.AppConfig
+var session *scs.SessionManager
+
 // main is the main application.
 func main() {
-	var app config.AppConfig
+	//Change app.InProduction to true when in production
+	app.InProduction = false
+
+	session = scs.New()
+	session.Lifetime = 24 * time.Hour
+	session.Cookie.Persist = true
+	session.Cookie.Secure = app.InProduction
+	session.Cookie.SameSite = http.SameSiteLaxMode
+
+	app.Session = session
 
 	tc, err := render.CreateTemplateCache()
 	if err != nil {
